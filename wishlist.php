c80a23cf44af27b8d2bc552d1f2f76f42a26d180
M <muhammetsevimli06@gmail.com>
2018-01-07 00:41:30 +0100

    version update
    version update

--- wishlist.php
+++ wishlist.php
@@ -3,7 +3,7 @@
  * Plugin Name: Woocommerce Wish List
  * Plugin URI: http://plife.se
  * Description: This plugin will add support for woocommerce to create wish list
- * Version: 1.0
+ * Version: 1.1
  * Author: M.Sevimli
  * Author URI: http://plife.se
  * License: GPL2
@@ -32,11 +32,17 @@ function boot_wishList() {
     global $product;
     $productIds=isset($_SESSION["wish_list"]) ? json_decode($_SESSION["wish_list"],true) : "[]";
     $className=isset($_SESSION["wish_list"]) && in_array($product->get_id(),$productIds) ? "choosenWhish" : "";
-
     $wLPosition= is_product_category('badrum') || is_product_tag('badrum') || is_product_category('badevaerelse') || is_product_tag('badevaerelse')  ? 'wList-bottom-right' :  get_option('wListPosition');
     echo '<div title="'.get_option("wListTitle").'" class="addWishListBtt '.$className.' '.$wLPosition.'" data="'.$product->get_id().'"><span></span></div>';
 }
 add_action("woocommerce_before_shop_loop_item","boot_wishList");
+function boot_wLSingle() {
+    global $product;
+    $productIdsS=isset($_SESSION["wish_list"]) ? json_decode($_SESSION["wish_list"],true) : "[]";
+    $classNameS=isset($_SESSION["wish_list"]) && in_array($product->get_id(),$productIdsS) ? "choosenWhish" : "";
+    echo '<div title="'.get_option("wListTitle").'" class="addWishListBtt wishButtonSingle '.$classNameS.'" data="'.$product->get_id().'"><span></span></div>';
+}
+add_action("woocommerce_after_add_to_cart_button","boot_wLSingle");
 
 function init_List() {
     ?>
@@ -50,6 +56,20 @@ function init_List() {
                 margin: 0 auto;
             }
         </style>
+        <?php
+            if(is_product()) {
+                ?>
+                    <style>
+                        .wishButtonSingle {
+                            margin-top: 21px;
+                            position: relative;
+                            width: 32px;
+                            margin-bottom: -20px;
+                        }
+                    </style>
+                <?php
+            }
+        ?>
         <div class="wishButton">
             <img class="bttImg" src="<?php echo plugins_url('inc/css/wishlist-Button.png', __FILE__)  ?>">
         </div>
